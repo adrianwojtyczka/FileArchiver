4ef1021106b8da39583a1548884dd955f3ef4d40
Adrian Wojtyczka <adrian.wojtyczka@hotmail.com>
2019-02-01 14:24:36 +0100

    Add project files.

---
+++ FileArchiver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileArchiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
